8a626f1aa0c2b41cd4a9962c6bb9c954d6c42299
lixu <lx507714@163.com>
2017-01-17 13:09:13 +0800

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TabbarComponent } from './common-view/tabbar/tabbar.component';
+import { FooterComponent } from './common-view/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
