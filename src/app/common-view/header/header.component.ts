701fa22666ceb680c1ebedf52ad11fd8b2c4138f
lishaohua <lsh294753@gmail.com>
2017-01-19 18:46:59 +0800

    不同路由的导航瞄点

--- src/app/common-view/header/header.component.ts
+++ src/app/common-view/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router, NavigationEnd} from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,13 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
 
   }
 
   linkToTitleById(id: string): void {
-    $("html, body").animate({scrollTop:$("#"+id).offset().top}, 1000);
+    let idLen = $("#"+ id).length;
+    if (idLen == 0) {
+      this.router.events.subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          this.router.navigate(["/home"]);
+          $("html, body").animate({scrollTop: $("#" + id).offset().top}, 500);
+        }
+      });
+    } else {
+      $("html, body").animate({scrollTop: $("#" + id).offset().top}, 500);
+    }
   }
 }
