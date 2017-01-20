f81a34b37aa41eae1e8c292f0886a64ce111ed01
lixu <lx507714@163.com>
2017-01-20 16:22:44 +0800

    finish swiper

--- src/app/common-view/home-swiper1/home-swiper1.component.ts
+++ src/app/common-view/home-swiper1/home-swiper1.component.ts
@@ -1,27 +1,26 @@
 import {Component, OnInit, AfterViewInit} from '@angular/core';
 
 @Component({
-    selector: 'app-home-swiper1',
-    templateUrl: './home-swiper1.component.html',
-    styleUrls: ['./home-swiper1.component.scss']
+  selector: 'app-home-swiper1',
+  templateUrl: './home-swiper1.component.html',
+  styleUrls: ['./home-swiper1.component.scss']
 })
 export class HomeSwiper1Component implements AfterViewInit,OnInit {
 
-    constructor() {
-    }
+  constructor() { }
 
-    ngAfterViewInit(): void {
-        let swap = new Swiper('.swap-list', {
-            pagination: '.swap-pagination',
-            slidesPerView: 3,
-            // spaceBetween: 30,
-            loop: false,
-            autoplay: 5000,
-            paginationClickable: true
-        });
-    }
+  ngAfterViewInit():void {
+    let swap = new Swiper('.swap-list', {
+      pagination: '.swap-pagination',
+      slidesPerView: 3,
+      // spaceBetween: 30,
+      loop: false,
+      autoplay: 5000,
+      paginationClickable: true
+    });
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
 }
